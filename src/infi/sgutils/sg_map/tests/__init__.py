c04c9cdbe52563d32e9796f97e49512c53050e85
Guy Rozendorn <guy@rzn.co.il>
2011-11-28 11:45:23 +0200

    work in progress

---
+++ src/infi/sgutils/sg_map/tests/__init__.py
@@ -0,0 +1,12 @@
+from infi import unittest
+
+class SgMapTestCase(unittest.TestCase):
+
+    def test_get_sg_to_hctl_mappings():
+        from .. import get_sg_to_hctl_mappings as getter
+        result = getter()
+
+    def test_get_sd_to_hctl_mappings():
+        from .. import get_sd_to_hctl_mappings as getter
+        result = getter()
+
