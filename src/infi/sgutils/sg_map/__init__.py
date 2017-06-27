621a00cfc6ea1b3ed7a83a682d2d241607c2df99
Arnon Yaari <arnony@infinidat.com>
2017-06-27 14:14:13 +0300

    STORAGEMODEL-393 look for hctl in /sys/class/scsi_disk if /sys/class/block doesn't exist

--- src/infi/sgutils/sg_map/__init__.py
+++ src/infi/sgutils/sg_map/__init__.py
@@ -1,12 +1,26 @@
 from infi.dtypes.hctl import HCTL
 import os
+import glob
 
 def get_hctl_for_sg_device(device_path):
     device_link = os.readlink(os.path.join(os.path.sep, 'sys', 'class', 'scsi_generic', os.path.basename(device_path), 'device'))
     host, channel, target, lun = os.path.basename(device_link).split(':')
     return HCTL(int(host), int(channel), int(target), int(lun))
 
+def get_hctl_for_sd_device_from_scsi_disk(device_path):
+    devices = glob.glob(os.path.join(os.path.sep, 'sys', 'class', 'scsi_disk', '*', 'device', 'block:' + os.path.basename(device_path)))
+    if not devices:
+        devices = glob.glob(os.path.join(os.path.sep, 'sys', 'class', 'scsi_disk', '*', 'device', 'block', os.path.basename(device_path)))
+        if not devices:
+            raise IOError()     # this is the exception _populate expects if HCTL not found
+    host, channel, target, lun = devices[0].split(os.path.sep)[4].split(':')
+    return HCTL(int(host), int(channel), int(target), int(lun))
+
 def get_hctl_for_sd_device(device_path):
+    if not os.path.exists(os.path.join(os.path.sep, 'sys', 'class', 'block')):
+        # older systems (rhel 5) don't have /sys/class/block but do have /sys/class/scsi_disk where
+        # we can search for the hctl anyway, maybe a little bit slower
+        return get_hctl_for_sd_device_from_scsi_disk(device_path)
     device_link = os.readlink(os.path.join(os.path.sep, 'sys', 'class', 'block', os.path.basename(device_path), 'device'))
     host, channel, target, lun = os.path.basename(device_link).split(':')
     return HCTL(int(host), int(channel), int(target), int(lun))
